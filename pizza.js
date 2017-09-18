341248ee9d15c2a530407a2626203f6a95025bc5
zb0831315 <zb0831315@otc.edu>
2017-09-18 13:13:05 -0500

    Created Pizza class

--- pizza.js
+++ pizza.js
@@ -1,4 +1,18 @@
 var inquirer = require('inquirer')
 const chalk = require('chalk')
+class Pizza {
+    constructor(size, sauce, toppings){
+        this.size = size
+        this.sauce = sauce
+        this.toppings = toppings
+    }
+    describe(){
+        return `This pizza is ${this.size} is, the sauce chosen is ${this.sauce},
+        and the toppings you picked are ${this.toppings}`
+    }
+}
 
+const pizza = new Pizza(22, 'tomato', ['sausage, cheese, bacon'])
 console.log(chalk.blue('Welcome to JT','s Pizza Shop!'))
+console.log(chalk.red('What would you like to order today?'))
+console.log(pizza.describe())

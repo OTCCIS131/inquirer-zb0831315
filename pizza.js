e702468888ece0f19a397b2ab63029f719e0c22e
zb0831315 <zb0831315@otc.edu>
2017-09-18 13:21:05 -0500

    Need to add inquirer

--- pizza.js
+++ pizza.js
@@ -1,5 +1,6 @@
 var inquirer = require('inquirer')
 const chalk = require('chalk')
+
 class Pizza {
     constructor(size, sauce, toppings){
         this.size = size
@@ -7,12 +8,14 @@ class Pizza {
         this.toppings = toppings
     }
     describe(){
-        return `This pizza is ${this.size} is, the sauce chosen is ${this.sauce},
-        and the toppings you picked are ${this.toppings}`
+        return `This pizza is ${this.size} is, the sauce chosen is ${this.sauce}, and the toppings you picked are ${this.toppings}.`
+    }
+    takeorder(){
+
     }
 }
 
-const pizza = new Pizza(22, 'tomato', ['sausage, cheese, bacon'])
+const pizza = new Pizza(22, 'tomato', ['cheese, bacon, sausage'])
 console.log(chalk.blue('Welcome to JT','s Pizza Shop!'))
 console.log(chalk.red('What would you like to order today?'))
 console.log(pizza.describe())

1f2c14b327687c4d0034eb77ce3e279368320d8b
zb0831315 <zb0831315@otc.edu>
2017-09-20 13:19:29 -0500

    Pizza order

--- pizza.js
+++ pizza.js
@@ -1,21 +1,16 @@
 var inquirer = require('inquirer')
 const chalk = require('chalk')
 
-class Pizza {
-    constructor(size, sauce, toppings){
-        this.size = size
-        this.sauce = sauce
-        this.toppings = toppings
-    }
-    describe(){
-        return `This pizza is ${this.size} is, the sauce chosen is ${this.sauce}, and the toppings you picked are ${this.toppings}.`
-    }
-    takeorder(){
-
-    }
-}
-
-const pizza = new Pizza(22, 'tomato', ['cheese, bacon, sausage'])
 console.log(chalk.blue('Welcome to JT','s Pizza Shop!'))
 console.log(chalk.red('What would you like to order today?'))
-console.log(pizza.describe())
+let questions = [
+    {
+    message: "What size pizza would you like?",
+    name:   "size",
+    type: "choices",
+    choices:["S", "M", "L"]
+    }]
+inquirer.prompt(questions).then(function (answers) 
+{
+    console.log(chalk.green(questions))
+})

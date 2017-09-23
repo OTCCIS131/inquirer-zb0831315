4450e88c2530978cfc2f10333b75e9100ea1e52e
zb0831315 <zb0831315@otc.edu>
2017-09-23 08:00:12 -0500

    Pizza order(almost finished)

--- pizza.js
+++ pizza.js
@@ -1,16 +1,39 @@
 var inquirer = require('inquirer')
 const chalk = require('chalk')
 
-console.log(chalk.blue('Welcome to JT','s Pizza Shop!'))
+console.log(chalk.blue('Welcome to My Super Aawesome Pizza Co.!'))
 console.log(chalk.red('What would you like to order today?'))
 let questions = [
     {
-    message: "What size pizza would you like?",
-    name:   "size",
-    type: "choices",
-    choices:["S", "M", "L"]
+        type: 'list',
+        name: 'size',
+        message: 'What size pizza would you like?',
+        choices: ['Small', 'Medium', 'Large'],
+        filter: function (val)
+        {
+            return val.toLowerCase()
+        }
+    },
+    {
+        type: 'rawlist',
+        name: 'sauces',
+        message: 'What kind of sauce would you like on your pizza?',
+        choices: ['Tomato', 'Alfredo', 'Marinana']
+    },
+    {
+        type: 'checkbox',
+        name: 'toppings',
+        message: 'What toppings would you like on your pizza?',
+        choices: ['Pepperoni', 'Cheese', 'Bacon', 'Sausage'] 
+    },
+    {
+        type: 'confrim',
+        name: 'isOrderCorrect',
+        message: 'Is this order correct?',
+        default: false
     }]
 inquirer.prompt(questions).then(function (answers) 
 {
-    console.log(chalk.green(questions))
+    console.log(chalk.bgMagenta('\nOrder receipt:'))
+    console.log(chalk.cyan(JSON.stringify(answers, null, '  ')))
 })
